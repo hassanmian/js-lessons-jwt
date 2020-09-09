49a89f6d2f991c5ef9d2c16472f026e1e0620212
Hassan Mian <hassanmian@hassans-imac.lan>
2020-09-09 11:14:32 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/"
+  const LOGIN_API = `${ROOT_URL}auth/api-token-auth/`
+  const EVENT_LIST_API = `${ROOT_URL}events/events/` 
+
+  const [token, setToken] = useState(null)
+  const [eventList, setEventList] = useState(null)
+  const [eventPageList, setEventPageList] = useState(null)
+
+  function logIn() {
+    const payload = {
+      email: 'test.user@willandskill.se',
+      password: 'js-lesson-10'
+    }
+    fetch(LOGIN_API, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+    .then(res => res.json())
+    .then(data => {
+      setToken(data.token)
+    })
+  }
+
+  function fetchEventList() {
+    fetch(EVENT_LIST_API, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      setEventList(data.results)
+    })
+  }
+
+  function fetchEventPages(event_slug) {
+    const url = `${ROOT_URL}cms/${event_slug}/pages`
+    fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(res=>res.json())
+    .then(data => {
+      setEventPageList(data.results)
+    })
+  }
+
+  useEffect(() => {
+    if(token) {
+      fetchEventList()
+    }
+  }, [token])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={logIn}>Login</button>
+      {eventList && eventList.map(eventItem => {
+        return (
+          <div key={eventItem.id}>
+            <h3>{eventItem.title}</h3>
+            <p>{ eventItem.description }</p>
+            <button onClick={()=>fetchEventPages(eventItem.slug)}>Get Event pages</button>
+            <hr/>
+          </div>
+        )
+      })}
+      <hr/>
+      <div>
+        {eventPageList && eventPageList.map(pageItem => {
+          return (
+            <div key={pageItem.id}>
+              <h5>{pageItem.title}</h5>
+              <iframe src={pageItem.backgroundVideoLink} />
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
